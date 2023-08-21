a223a2acb538e495faed4e03b39a67f456c5df99
Vlad <repovlad@gmail.com>
2023-08-21 21:26:45 +0300

    reverse-int-comlpete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+        return (
+            parseFloat(n.toString().split('').reverse().join(''))
+            ) * Math.sign(n) 
+    } else {
+        return (
+            parseFloat(n.toString().split('').reverse().join(''))
+            ) * Math.sign(n) * -1
+    }
 }
+
